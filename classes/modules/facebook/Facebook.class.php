a0ec62396c05325f69c7197ee499c10075bca90b
Sergey Marin <marin.sergey@gmail.com>
2010-11-22 00:52:06 +0300

    killing bugs

--- classes/modules/facebook/Facebook.class.php
+++ classes/modules/facebook/Facebook.class.php
@@ -21,7 +21,7 @@ class PluginFacebook_ModuleFacebook extends Module {
 	 */
 	public function Init() {
         // facebook API
-        $path=Plugin::GetTemplateWebPath('facebook').'classes/lib/facebook-php-sdk/src/facebook.php';
+        $path=Plugin::GetPath('facebook').'classes/lib/facebook-php-sdk/src/facebook.php';
         include $path;
         
         $this->aCfg=Config::Get('plugin.facebook');
