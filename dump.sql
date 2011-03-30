357a08405be0ab6ea083be0f8d4cdd450436454d
Sergey Marin <sergeym@fly4fun.ru>
2011-03-30 11:32:27 +0400

    - sql dump fix

--- dump.sql
+++ dump.sql
@@ -6,7 +6,9 @@ CREATE TABLE IF NOT EXISTS `prefix_plugin_facebook_topic_list` (
   CONSTRAINT `FK_plugin_facebook_topic` FOREIGN KEY (`topic_id`) REFERENCES `prefix_topic` (`topic_id`) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `sm_plugin_facebook_settings` (
+ALTER TABLE `prefix_plugin_facebook_topic_list`  ADD COLUMN `status` ENUM('published','published') NOT NULL AFTER `publish_id`;
+
+CREATE TABLE IF NOT EXISTS `sm_plugin_facebook_settings` (
 	`id` INT(10) NULL DEFAULT NULL,
 	`appId` CHAR(20) NULL DEFAULT NULL,
 	`appKey` CHAR(48) NULL DEFAULT NULL,
